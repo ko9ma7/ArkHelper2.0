2cbfe437e09714fff9e29f516511cdaffba6da67
邶柒EvATive7 <evative7@gmail.com>
2022-11-04 22:28:11 +0800

    寻访

--- Xaml/UserData_Gacha.xaml.cs
+++ Xaml/UserData_Gacha.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using Newtonsoft.Json.Linq;
 using RestSharp;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
+using System.Diagnostics;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Windows.UI.Xaml.Controls;
 using Page = System.Windows.Controls.Page;
 
 namespace ArkHelper.Xaml
@@ -27,99 +15,34 @@ namespace ArkHelper.Xaml
     /// </summary>
     public partial class UserData_Gacha : Page
     {
-        public class ArknightsUser
-        {
-            public string Name { get; set; }
-            public string UID { get; set; }
-            public ArknightsUser()
-            {
-
-            }
-        }
         /// <summary>
         /// Token
         /// </summary>
-        public static string Token = null;
+        public string Token = null;
         /// <summary>
         /// 抽卡列表
         /// </summary>
-        public static List<JsonElement> Lists = new List<JsonElement>();
-        /// <summary>
-        /// 报错
-        /// </summary>
-        public enum errorKind
-        {
-            token_ineffective,
-            token_null,
-            password_error,
-            need_password,
-            CD,
-            succeed,
-            need_cap
-        }
+        public List<JsonElement> Lists = new List<JsonElement>();
 
-
-        /// <summary>
-        /// 是否处于CD（避免被和谐）
-        /// </summary>
-        private static bool ableToGet = true;
-        /// <summary>
-        /// 根据账号密码获取token
-        /// </summary>
-        private static errorKind _FromUserAndPassword()
-        {
-            //获取token
-            var request = new RestRequest
-            {
-                Method = Method.Post,
-                RequestFormat = RestSharp.DataFormat.Json
-            };
-            var json = new JObject()
-                    {
-                        {"phone",Data.UserData.User },
-                        {"password",Data.UserData.Password }
-                    };
-            request.AddParameter("", json, ParameterType.RequestBody);
-            var client = new RestClient("https://as.hypergryph.com/user/auth/v1/token_by_phone_password");
-            var result = client.Post(request);
-            var getJson = JsonDocument.Parse(result.Content).RootElement;
-            switch (getJson.GetProperty("status").GetInt32())
-            {
-                case 1:
-                    return errorKind.need_cap;
-                case 100:
-                    return errorKind.password_error;
-                case 0:
-                    Token = getJson.GetProperty("data").GetProperty("token").GetString();
-                    break;
-            }
-            return errorKind.succeed;
-        }
-
+        #region List
         /// <summary>
         /// 获取单页抽卡列表
         /// </summary>
         /// <param name="page"></param>
         /// <returns></returns>
-        private static JsonElement get(int page)
+        private JsonElement get(int page)
         {
             //获取结果
             var client = new RestClient("https://ak.hypergryph.com/user/api/inquiry/gacha?page=" + page + "&token=" + Token + "&channelId=1");
             var result = client.Get(new RestRequest());
             return JsonDocument.Parse(result.Content).RootElement;
         }
-
         /// <summary>
         /// 根据token获取抽卡列表
         /// </summary>
-        private static void GetResult()
+        private void GetResult()
         {
-            ableToGet = false;
-            Task.Run(() =>
-            {
-                Thread.Sleep(60000);
-                ableToGet = true;
-            });
+            Lists.Clear();
             int totalPage = get(1).GetProperty("data").GetProperty("pagination").GetProperty("total").GetInt32();
             if (totalPage == 0) return;
             else
@@ -134,33 +57,27 @@ namespace ArkHelper.Xaml
                 }
             }
         }
+        #endregion
 
         /// <summary>
-        /// 预处理返回错误情况
+        /// 测试token是否可用 否返回无效报错
         /// </summary>
         /// <returns>错误类型</returns>
-        public static errorKind Get()
+        public bool IsTokenUseful()
         {
-            if (!ableToGet) return errorKind.CD;
-            if (Token == null)
-            {
-                if (Data.UserData.User == "" || Data.UserData.Password == "")
-                {
-                    return errorKind.need_password;
-                }
-                else
-                {
-                    var aab = _FromUserAndPassword();
-                    if (aab != errorKind.succeed) return aab;
-                }
-            }
-
-            //测试token
             var userdata = WithNet.GetFromApi("https://as.hypergryph.com/user/info/v1/basic?token=" + Token);
-            if (!userdata.TryGetProperty("error", out var error)) return errorKind.token_ineffective;
+            if (!userdata.TryGetProperty("error", out var error)) return false; //Token无效返回
+            return true;
+        }
 
+        private void FreshData()
+        {
+            if (IsTokenUseful())
+            {
+                Oauth.Visibility = Visibility.Visible;
+                return;
+            }
             GetResult();
-            return errorKind.succeed;
         }
 
         #region UI
@@ -168,35 +85,46 @@ namespace ArkHelper.Xaml
         {
             InitializeComponent();
         }
+
         private void Fresh(object sender, RoutedEventArgs e)
         {
-            _Fresh();
+            FreshData();
         }
-        private void _Fresh()
+
+        #region
+        private void FT_1(object sender, RoutedEventArgs e)
         {
-            Unable.Visibility = ableToGet ? Visibility.Collapsed : Visibility.Visible;
-            var aaa = Get();
+            Process.Start("https://ak.hypergryph.com/user/login");
         }
-        private void FromUserAndPassword(object sender, RoutedEventArgs e)
+        private void FT_2(object sender, RoutedEventArgs e)
         {
-            Data.UserData.User = User.Text;
-            Data.UserData.Password = Password.Password;
-            var aa = _FromUserAndPassword();
-            switch (aa)
-            {
-                case errorKind.succeed:
-                    FromUser.Visibility = Visibility.Collapsed;
-                    break;
-                case errorKind.password_error:
-                    WithSystem.Message("账号或密码错误", "请重新输入");
-                    break;
-                case errorKind.need_cap:
-                    WithSystem.Message("单位时间查询次数已达到上限", "请稍等");
-                    break;
-            }
+            Process.Start("https://web-api.hypergryph.com/account/info/hg");
         }
         #endregion
+        private void FromTokenJson(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var aa = JsonSerializer.Deserialize<JsonElement>(TokenJsonTextBox.Text);
+                Token = aa.GetProperty("data").GetProperty("content").GetString();
+                //FromToken.Visibility = Visibility.Collapsed;
+                Oauth.Visibility = Visibility.Collapsed;
+            }
+            catch
+            {
+                TokenOauthButton.Content = "输入错误，请重新输入......";
+                Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (TokenOauthButton.Content.ToString() == "输入错误，请重新输入......")
+                            TokenOauthButton.Content = "认证";
+                    });
+                });
+            }
 
-
+        }
+        #endregion
     }
 }
